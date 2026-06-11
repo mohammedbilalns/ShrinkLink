638a394ea70384146af6d2effd34af2f7428c81e
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-06-11 15:00:03 +0530

    chore : configured mongo and redis

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/joho/godotenv"
 	"github.com/mohammedbilalns/shrinklink/internal/config"
+	"github.com/mohammedbilalns/shrinklink/internal/db"
 )
 
 
@@ -16,4 +18,22 @@ func main(){
 		log.Println(".env file not found ")
 	}
 	cfg := config.Load()
+
+	log.Println("Config Loaded")
+
+	mongoClient, err := db.ConnectMongo(cfg.MongoURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer mongoClient.Disconnect(context.Background())
+
+	log.Println("Mongo Connected")
+
+	redisClient, err := db.ConnectRedis(cfg.RedisURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer redisClient.Close()
+
+	log.Println("Redis Connected")
 }
