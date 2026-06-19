55b8fecc30fea410d29e55fd4114d5e99072e41d
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-06-19 18:42:41 +0530

    feat : configured url repository

--- backend/internal/repository/url_repository.go
+++ backend/internal/repository/url_repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/mohammedbilalns/shrinklink/internal/model"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type ShortURLRepository interface {
@@ -17,11 +18,22 @@ type ShortURLRepository interface {
 		shortURL string, 
 	) (*model.ShortURL, error)
 
-	FindAllByUser(
+	FindByShortURLAndIncrementClicks(
 		ctx context.Context,
-		userId string,
-		skip int,
-		limit int, 
-	) ([]model.ShortURL, error)
+		shortURL string,
+	)(*model.ShortURL, error)
+
+	FindByUser(
+		ctx context.Context,
+		userId bson.ObjectID,
+		skip int64,
+		limit int64, 
+	) ([]model.ShortURL, int64, error)
+
+	FindExistingURL(
+		ctx context.Context,
+		userID bson.ObjectID,
+		fullURL string,
+	)(*model.ShortURL, error)
 
 }
