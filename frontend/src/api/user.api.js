b8d2e76a655db04d67f2ff1cce7deddc6167325a
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-06-24 14:35:56 +0530

    chore : updated client api end points

--- frontend/src/api/user.api.js
+++ frontend/src/api/user.api.js
@@ -1,32 +1,32 @@
 import axiosInstance from "../utils/axiosInstance"
 
 export const loginUser = async (email, password) =>{
-	const {data} = await axiosInstance.post('/api/auth/login', {email, password})
+	const {data} = await axiosInstance.post('/auth/login', {email, password})
 	return data 
 }
 
 export const registerUser = async(name, email , password) => {
-	const {data} = await axiosInstance.post('/api/auth/register', {name, email , password})
+	const {data} = await axiosInstance.post('/auth/register', {name, email , password})
 	return data
 }
 
 export const verifyOtp = async(email, otp) => {
-	const {data} = await axiosInstance.post('/api/auth/verify', {email, otp})
+	const {data} = await axiosInstance.post('/auth/verify', {email, otp})
 	return data
 }
 
 export const resendOtp = async(email) => {
-	const {data} = await axiosInstance.post('/api/auth/resend', {email})
+	const {data} = await axiosInstance.post('/auth/resend', {email})
 	return data
 }
 
 export const logoutUser = async()=> {
-	const {data} = await axiosInstance.get('/api/auth/logout')
+	const {data} = await axiosInstance.get('/auth/logout')
 	return data 
 }
 
 export const getCurrentUser = async()=> {
-	const {data} = await axiosInstance.get('/api/auth/me')
+	const {data} = await axiosInstance.get('/auth/me')
 	return data 
 }
 
